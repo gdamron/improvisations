99b58c41494bc7a857e67f261b2f1fbbb9d6f42d
Grant Damron <grant@ilusiv.io>
2025-08-17 10:33:52 -0700

    update gibberish

--- 2018-03-05/gibberish.js
+++ 2018-03-05/gibberish.js
@@ -7,85 +7,94 @@ const BASE_NOTE = 36;
 const INTERVAL = 7;
 const BASE_DUR = 8;
 
-const DURS = [0.125, 0.25, 0.5]
+const DURS = [0.125, 0.25, 0.5];
 
-const PATTERN = [
-    0, 7, 14, 21, 28, 35, 42
-];
+const PATTERN = [0, 7, 14, 21, 28, 35, 42];
 
-const WAVE = ['sine', 'saw', 'triangle']
+const WAVE = ["sine", "saw", "triangle"];
 
-window.onload = function() {
-    Gibberish.init();
-    Gibberish.export(window);
-    tune();
-}
+var isSoundInit = false;
 
-window.tune = function() {
-    var control = {
-        sound: sound,
-        time: time
-    };
-    
-    var reverb = Freeverb({
-        roomSize: 1.0,
-        damping: 0.01,
-        dry: 0,
-        wet1: 0.6,
-    }).connect();
+window.onmouseup = function () {
+  if (isSoundInit) {
+    return;
+  }
 
-    var synths = [];
-    for (let i = 0; i < SYNTH_COUNT; i++) {
-        const wave = Math.floor(Math.random() * WAVE.length);
-        const synth = createSynth(wave);
-        synth.connect(reverb);
-        synths.push(synth);
-    }
+  isSoundInit = true;
+  Gibberish.init();
+  Gibberish.export(window);
+  tune();
+};
 
-    seq = Gibberish.Sequencer({
-        target: control,
-        key: 'sound',
-        values: synths,
-        timings: [time],
-    }).start();
+window.tune = function () {
+  var control = {
+    sound: sound,
+    time: time,
+  };
+
+  var reverb = Freeverb({
+    roomSize: 1.0,
+    damping: 0.01,
+    dry: 0,
+    wet1: 0.6,
+  }).connect();
+
+  var synths = [];
+  for (let i = 0; i < SYNTH_COUNT; i++) {
+    const wave = Math.floor(Math.random() * WAVE.length);
+    const synth = createSynth(wave);
+    synth.connect(reverb);
+    synths.push(synth);
+  }
+
+  seq = Gibberish.Sequencer({
+    target: control,
+    key: "sound",
+    values: synths,
+    timings: [time],
+  }).start();
 };
 
 function createSynth(wave) {
-    return PolySynth({
-        waveform: WAVE[wave],
-        attack:SR/8,
-        decay:SR * BASE_DUR,
-        filterType: 1,
-        filterMode: 0,
-        cutoff: 0.01,
-        filterMult: 9,
-        saturation: 22,
-        Q: 0.6,
-        resonance: 9.85,
-        antialias: true,
-        gain: 0.5,
-        maxVoices: VOICE_COUNT
-    });
+  return PolySynth({
+    waveform: WAVE[wave],
+    attack: SR / 8,
+    decay: SR * BASE_DUR,
+    filterType: 1,
+    filterMode: 0,
+    cutoff: 0.01,
+    filterMult: 9,
+    saturation: 22,
+    Q: 0.6,
+    resonance: 9.85,
+    antialias: true,
+    gain: 0.5,
+    maxVoices: VOICE_COUNT,
+  });
 }
 
 function sound(synth) {
-    var freq;
-    while(!freq || synth.voices.find(function(f) { return f.frequency === freq })) {
-        var index = Math.floor(Math.random() * PATTERN.length);
-        var degree = BASE_NOTE + PATTERN[index];
-        freq = midiToFreq(degree);
-    }
+  var freq;
+  while (
+    !freq ||
+    synth.voices.find(function (f) {
+      return f.frequency === freq;
+    })
+  ) {
+    var index = Math.floor(Math.random() * PATTERN.length);
+    var degree = BASE_NOTE + PATTERN[index];
+    freq = midiToFreq(degree);
+  }
 
-    synth.note(freq);
-
+  synth.note(freq);
 }
 
 function time() {
-    var index = Math.floor(Math.random() * DURS.length);
-    var multiplier = DURS[index]
-    return BASE_DUR * multiplier * SR;
+  var index = Math.floor(Math.random() * DURS.length);
+  var multiplier = DURS[index];
+  return BASE_DUR * multiplier * SR;
 }
 
 function midiToFreq(midi) {
-    return 440 * Math.pow(2, (midi - 69)/12.0);
+  return 440 * Math.pow(2, (midi - 69) / 12.0);
 }
--- 2021-02-15/gibberish.js
+++ 2021-02-15/gibberish.js
@@ -8,136 +8,146 @@ const BASE_DUR = Math.random() * 0.1 + 0.1;
 const PATTERN_GEN_INTERVAL = 20000;
 const MIN_WEIGHT = 1;
 const MAX_WEIGHT = 5;
-const BASE_PATTERN = [
-    -BASE_NOTE, -4, -2, 0, 3, 5, 7, 10, 12, 14, 15, 17, 19
-];
+const BASE_PATTERN = [-BASE_NOTE, -4, -2, 0, 3, 5, 7, 10, 12, 14, 15, 17, 19];
 
 var DURS = generateDurs();
 var PATTERN = [];
 
-window.onload = function() {
-    Gibberish.init();
-    Gibberish.export(window);
-    generatePatternPeriodically();
-    tune();
-}
+var isSoundInit = false;
 
-window.tune = function() {
-    var control = {
-        sound: sound,
-        time: time
-    };
+window.onmouseup = function () {
+  if (isSoundInit) {
+    return;
+  }
 
-    const params = generateSynthParams();
-    console.log(params);
+  isSoundInit = true;
 
-    var reverb = Freeverb({
-        roomSize: 1.0,
-        damping: params.damping,
-        dry: 0.01,
-        wet1: 0.6,
-    }).connect();
+  Gibberish.init();
+  Gibberish.export(window);
+  generatePatternPeriodically();
+  tune();
+};
 
-    var synths = [];
+window.tune = function () {
+  var control = {
+    sound: sound,
+    time: time,
+  };
 
-    const baseGain = 1.0 / VOICE_COUNT;
-    for (let i = 0; i < SYNTH_COUNT; i++) {
-        const synth = createSynth(params.waveform, params.gain * baseGain, params.attack);
-        synth.connect(reverb);
-        synths.push(synth);
-    }
+  const params = generateSynthParams();
+  console.log(params);
 
-    seq = Gibberish.Sequencer({
-        target: control,
-        key: 'sound',
-        values: synths,
-        timings: [time],
-    }).start();
+  var reverb = Freeverb({
+    roomSize: 1.0,
+    damping: params.damping,
+    dry: 0.01,
+    wet1: 0.6,
+  }).connect();
+
+  var synths = [];
+
+  const baseGain = 1.0 / VOICE_COUNT;
+  for (let i = 0; i < SYNTH_COUNT; i++) {
+    const synth = createSynth(
+      params.waveform,
+      params.gain * baseGain,
+      params.attack,
+    );
+    synth.connect(reverb);
+    synths.push(synth);
+  }
+
+  seq = Gibberish.Sequencer({
+    target: control,
+    key: "sound",
+    values: synths,
+    timings: [time],
+  }).start();
 };
 
 function createSynth(wave, gain, attack) {
-    return PolySynth({
-        waveform: wave,
-        gain: gain,
-        maxVoices: VOICE_COUNT,
-        attack: attack
-    });
+  return PolySynth({
+    waveform: wave,
+    gain: gain,
+    maxVoices: VOICE_COUNT,
+    attack: attack,
+  });
 }
 
 function sound(synth) {
-    const notes = generateNotes();
-    synth.chord(notes);
+  const notes = generateNotes();
+  synth.chord(notes);
 }
 
 function time() {
-    var index = Math.floor(window.getNoise() * DURS.length);
-    var multiplier = DURS[index]
-    return BASE_DUR * multiplier * SR;
+  var index = Math.floor(window.getNoise() * DURS.length);
+  var multiplier = DURS[index];
+  return BASE_DUR * multiplier * SR;
 }
 
 function midiToFreq(midi) {
-    return 440 * Math.pow(2, (midi - 69)/12.0);
+  return 440 * Math.pow(2, (midi - 69) / 12.0);
 }
 
 function generateSynthParams() {
-    const waves = ['sine', 'triangle', 'pwm', 'square'];
-    const gains = [1.0, 0.8, 0.2, 0.1];
-    const dampings = [0.6, 0.2, 0.01, 0.01];
-    const attacks = [256, 128, 64, 64];
+  const waves = ["sine", "triangle", "pwm", "square"];
+  const gains = [1.0, 0.8, 0.2, 0.1];
+  const dampings = [0.6, 0.2, 0.01, 0.01];
+  const attacks = [256, 128, 64, 64];
 
-    const index = Math.floor(Math.random() * waves.length);
-    const waveform = waves[index];
-    const gain = gains[index];
-    const damping = dampings[index];
-    const attack = attacks[index];
+  const index = Math.floor(Math.random() * waves.length);
+  const waveform = waves[index];
+  const gain = gains[index];
+  const damping = dampings[index];
+  const attack = attacks[index];
 
-    return {
-        waveform,
-        gain,
-        damping,
-        attack
-    };
+  return {
+    waveform,
+    gain,
+    damping,
+    attack,
+  };
 }
 
 function generatePattern() {
-    let notes = [];
+  let notes = [];
 
-    for (const note of BASE_PATTERN) {
-        let cnt = Math.floor(Math.random() * MAX_WEIGHT) + MIN_WEIGHT;
-        while (cnt > 0) {
-            notes.push(note);
-            cnt--;
-        }
+  for (const note of BASE_PATTERN) {
+    let cnt = Math.floor(Math.random() * MAX_WEIGHT) + MIN_WEIGHT;
+    while (cnt > 0) {
+      notes.push(note);
+      cnt--;
     }
+  }
 
-    return notes;
+  return notes;
 }
 
 function generatePatternPeriodically() {
-    PATTERN = generatePattern();
-    setTimeout(generatePatternPeriodically, PATTERN_GEN_INTERVAL);
+  PATTERN = generatePattern();
+  setTimeout(generatePatternPeriodically, PATTERN_GEN_INTERVAL);
 }
 
 function generateDurs() {
-    return [0.5, 1, 2];
+  return [0.5, 1, 2];
 }
 
 function generateNotes() {
-    const r = Math.floor(random(PATTERN.length));
-    const n = Math.floor(window.getNoise() * PATTERN.length);
-    var index = Math.floor(0.45 * r + 0.55 * n);
-    var degree = BASE_NOTE + PATTERN[index];
-    var freq = midiToFreq(degree);
+  const r = Math.floor(random(PATTERN.length));
+  const n = Math.floor(window.getNoise() * PATTERN.length);
+  var index = Math.floor(0.45 * r + 0.55 * n);
+  var degree = BASE_NOTE + PATTERN[index];
+  var freq = midiToFreq(degree);
 
-    let notes = [freq, freq*1.5, freq*2];
+  let notes = [freq, freq * 1.5, freq * 2];
 
-    if (Math.random() > 0.75) {
-        notes = notes.splice(1);
-    }
+  if (Math.random() > 0.75) {
+    notes = notes.splice(1);
+  }
 
-    if (Math.random() > 0.4) {
-        notes = notes.splice(0);
-    }
+  if (Math.random() > 0.4) {
+    notes = notes.splice(0);
+  }
 
-    return notes;
+  return notes;
 }
